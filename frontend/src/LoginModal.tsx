7e63f3e41ebc7d5765f94d14525d943908572637
Shreyas Sawant <shreyassawant018@gmail.com>
2024-04-22 02:09:04 -0400

    login and register toggle

--- frontend/src/LoginModal.tsx
+++ frontend/src/LoginModal.tsx
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import axiosClient from "./utils/axiosClient";
 
 interface LoginModalProps {
-  onClose: () => void;
+  onLogin: () => void;
+  onRegister: () => void;
 }
-
-const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
+interface LoginFormData {
+  username: string;
+  password: string;
+}
+const LoginModal: React.FC<LoginModalProps> = ({ onLogin, onRegister }) => {
+  const client = axiosClient();
+  const [loginForm, setLoginForm] = useState<LoginFormData>({
+    username: "",
+    password: "",
+  });
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: keyof LoginFormData
+  ) => {
+    setLoginForm({
+      ...loginForm,
+      [field]: event.target.value,
+    });
+  };
+  const handleLogin = async () => {
+    try {
+      const payload = JSON.stringify(loginForm);
+      const response = await client.post("/users/login", payload);
+      console.log(`User Registered: ${response.data}`);
+      onLogin();
+    } catch (error) {
+      console.log(`User Registration failed: ${error}`);
+    }
+  };
   return (
-    <div className="modal login-modal">
-      <div className="modal-content">
+    <div>
+      <div>
         <h2>Login</h2>
         <form>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
+          <label htmlFor="username">Username or Email:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={loginForm.username}
+            onChange={(e) => handleChange(e, "username")}
+          />
+          <br />
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-          <button type="button">Login</button>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={loginForm.password}
+            onChange={(e) => handleChange(e, "password")}
+          />
+          <br />
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
         </form>
-        <button onClick={onClose}>Close</button>
+        <button onClick={onRegister}>Register</button>
       </div>
     </div>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import App from "./App1.tsx";
 import Registration from "./registration.tsx";
 import "./index.css";
+import LoginModal from "./LoginModal.tsx";
 
 const Main: React.FC = () => {
   const [sessionActive, setSessionActive] = useState(false);
-
+  const [login, setLogin] = useState(true);
   const handleRegistration = () => {
+    setLogin(true);
+    setSessionActive(false);
+  };
+  const handleLogin = () => {
+    setLogin(false);
     setSessionActive(true);
   };
-
+  const handleRegister = () => {
+    setLogin(false);
+    setSessionActive(false);
+  };
   return (
     <React.StrictMode>
       {sessionActive ? (
         <App />
+      ) : login ? (
+        <LoginModal onLogin={handleLogin} onRegister={handleRegister} />
       ) : (
         <Registration onRegister={handleRegistration} />
       )}
