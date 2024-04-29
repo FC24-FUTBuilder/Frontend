3d6dd71df3dc2ce35e245aa172c507040e9da239
Shreyas Sawant <shreyassawant018@gmail.com>
2024-04-28 20:56:58 -0400

    type check disabled

--- frontend/src/registration.tsx
+++ frontend/src/registration.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axiosClient from "./utils/axiosClient";
 import "./Registration.css";
 interface Props {
@@ -38,49 +38,61 @@ const Registration: React.FC<Props> = ({ onRegister }) => {
   };
   return (
     <div>
-                  <video className="background-video" autoPlay loop muted >
-  <source src="src/assets/reg.mp4" type="video/mp4"/>
-</video>
+      <video className="background-video" autoPlay loop muted>
+        <source src="src/assets/reg.mp4" type="video/mp4" />
+      </video>
 
-    <div className="form-box">
-      <br/>
-      <h2>Registration Form</h2>
-      <form>
-        <label className="plabel" htmlFor="username">Username</label><br/>
-        <input
-        className="inputfield"
-          type="text"
-          id="username"
-          name="username"
-          value={userDetails.username}
-          onChange={(e) => handleChange(e, "username")}
-        />
-        <br /><br/>
-        <label className="elabel" htmlFor="email">Email</label><br/>
-        <input
-        className="inputfield"
-          type="email"
-          id="email"
-          name="email"
-          value={userDetails.email}
-          onChange={(e) => handleChange(e, "email")}
-        />
-        <br /><br/>
-        <label className="plabel" htmlFor="password">Password</label><br/>
-        <input
-        className="inputfield"
-          type="password"
-          id="password"
-          name="password"
-          value={userDetails.password}
-          onChange={(e) => handleChange(e, "password")}
-        />
-        <br /><br/>
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
-    </div>
+      <div className="form-box">
+        <br />
+        <h2>Registration Form</h2>
+        <form>
+          <label className="plabel" htmlFor="username">
+            Username
+          </label>
+          <br />
+          <input
+            className="inputfield"
+            type="text"
+            id="username"
+            name="username"
+            value={userDetails.username}
+            onChange={(e) => handleChange(e, "username")}
+          />
+          <br />
+          <br />
+          <label className="elabel" htmlFor="email">
+            Email
+          </label>
+          <br />
+          <input
+            className="inputfield"
+            type="email"
+            id="email"
+            name="email"
+            value={userDetails.email}
+            onChange={(e) => handleChange(e, "email")}
+          />
+          <br />
+          <br />
+          <label className="plabel" htmlFor="password">
+            Password
+          </label>
+          <br />
+          <input
+            className="inputfield"
+            type="password"
+            id="password"
+            name="password"
+            value={userDetails.password}
+            onChange={(e) => handleChange(e, "password")}
+          />
+          <br />
+          <br />
+          <button type="button" onClick={handleRegister}>
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
